4c532d96e4e71ce3c71b27dac90049938f01dbbe
darionicolas06 <darionicolas06@users.noreply.github.com>
2020-12-20 20:13:24 -0300

    si
    si

---
+++ js/SolicitudDo.js
@@ -0,0 +1,10 @@
+function contrata(){
+   document.getElementById('ausente').style.display="none";
+}
+
+function reemplazo(){
+    document.getElementById('ausente').style.display="block";
+ }
+
+ 
+ 
