25b501595b2f47e9b96e718d08ca8ee3c282d4ba
seba <sebas.a.nunez@gmail.com>
2020-12-20 20:00:41 -0300

    agrega funcion de rellenar datos

--- SolicitudDo.js
+++ SolicitudDo.js
@@ -6,5 +6,38 @@ function reemplazo(){
     document.getElementById('ausente').style.display="block";
  }
 
- 
- 
+
+async function rellenarDatos(){
+   let rut = document.getElementById("rut").value
+   let nombre = document.getElementById("nombre")
+   let apellidop = document.getElementById("apellidop")
+   let apellidom = document.getElementById("apellidom")
+   let fechanac = document.getElementById("fechanac")
+   let ciudad = document.getElementById("ciudad")
+   let direccion = document.getElementById("direccion")
+   let telefono = document.getElementById("telefono")
+   
+   let profe = await buscarProfesPorRut(rut)
+
+   nombre.value = profe.nombre
+   apellidop.value = profe.paterno
+   apellidom.value = profe.materno
+   fechanac.value = profe.fecha_nacim
+   ciudad.value = profe.ciudad
+   direccion.value = profe.direccion
+   telefono.value = profe.telefono
+}
+
+// fetch es una funcion asincrona, para usarla tiene que estar en otra funcion async al igual que .json()
+async function buscarProfesPorRut( rut ){
+   let response = await fetch("http://64.225.53.176:8000/api/v0/profes/")
+   let resToJSON = await response.json()
+   let next = resToJSON.next
+   let results = resToJSON.results
+
+   for (let profe of results) {
+      if (profe.rut == rut) {
+         return profe
+      }
+   }
+}
